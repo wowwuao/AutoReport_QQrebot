cec8ff7db8886b23760fe48f57a0c03c5febea5c
Wow <57665242+wowwuao@users.noreply.github.com>
2021-08-09 09:03:30 +0800

    Update setTime.py

--- QQRebot/src/plugins/atall/plugins/setTime.py
+++ QQRebot/src/plugins/atall/plugins/setTime.py
@@ -4,7 +4,7 @@ from autoReport import autoReport
 
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
-
+#装饰器的参数为定时时间
 @scheduler.scheduled_job('cron', hour='00', minute='01', id='autoRp1')
 @scheduler.scheduled_job('cron', hour='08', minute='00', id='autoRp2')
 @scheduler.scheduled_job('cron', hour='09', minute='00', id='autoRp3')
@@ -17,6 +17,6 @@ async def handle_first_receive():
     a5 = autoReport("学号", "密码", "姓名")
     a6 = autoReport("学号", "密码", "姓名")
     return await bot.call_api('send_msg', **{
-        'message': a1+'\n'+'\n'+a2+'\n'+a3+'\n'+a4+'\n'+a5+'\n'+a6,
+        'message': a1+'\n'+a2+'\n'+a3+'\n'+a4+'\n'+a5+'\n'+a6,
         'group_id': '群号'
     })
